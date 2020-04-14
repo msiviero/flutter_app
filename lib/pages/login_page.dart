5f1c621c9f36f1bbb119fa1e35b37c26290db871
msiviero <m.siviero83@gmail.com>
2020-04-14 14:55:47 +0200

    Import optimization

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import "package:flutter/material.dart";
-import 'package:flutter_learning/state/login_objects.dart';
 import 'package:flutter_learning/state/login_state.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
